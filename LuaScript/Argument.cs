4165a2525ff8df6d16715e1621d388369cb16686
yfdyzjt <yfdyzjt@gmail.com>
2024-06-19 18:16:00 +0800

    Add FormatArgs

---
+++ LuaScript/Argument.cs
@@ -0,0 +1,35 @@
+﻿using Neo.IronLua;
+
+namespace TMake.LuaScript
+{
+    public static partial class Root
+    {
+        public static void FormatArgs(LuaTable self, string[] args, params string[] pairs)
+        {
+            for (int i = 0; i < pairs.Length; i++)
+            {
+                var parts = pairs[i].Split(':', 2);
+
+                string name = parts[0].Trim();
+                string type = parts.Length == 2 ? parts[1].Trim() : "";
+
+                if (type == "")
+                {
+                    self[name] = args[i];
+                }
+                else
+                {
+                    try
+                    {
+                        Type targetType = Type.GetType(type, true, true) ?? typeof(string);
+                        self[name] = Convert.ChangeType(args[i], targetType);
+                    }
+                    catch
+                    {
+                        self[name] = args[i];
+                    }
+                }
+            }
+        }
+    }
+}
