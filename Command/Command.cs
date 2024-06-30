25268ed58ecbc0add87483fcfa8db39d9a8813de
yfdyzjt <yfdyzjt@gmail.com>
2024-06-30 21:02:55 +0800

    Fix Bug

--- Command/Command.cs
+++ Command/Command.cs
@@ -1,32 +1,79 @@
-﻿namespace TMake
+﻿using System.Reflection;
+
+namespace TMake
 {
     public class Command
     {
-        public static void Execute(string[] cmdargs)
+        public static object? Execute(string[] args)
         {
-            if (cmdargs.Length == 0)
+            if (args.Length == 0)
             {
-                cmdargs = [
+                args = [
                     "Run",
                     "tmake.lua",
                 ];
             }
 
-            var methodName = cmdargs[0];
-            var callargs = new string[cmdargs.Length - 1];
-            Array.Copy(cmdargs, 1, callargs, 0, callargs.Length);
+            var methodName = args[0];
+            var strargs = new string[args.Length - 1];
+            var objargs = new List<object>();
 
+            Array.Copy(args, 1, strargs, 0, strargs.Length);
             var @delegate = Reflective.CreateDelegate(Reflective.GetMethods("TMake.LuaScript")
-                .First(method => method.Name.Equals(methodName, StringComparison.OrdinalIgnoreCase)));
+                .Where(method => method.Name.Equals(methodName, StringComparison.OrdinalIgnoreCase))
+                .First(method => ParameterMatch(method.GetParameters(), strargs, objargs)));
 
             if (@delegate != null)
             {
-                @delegate.DynamicInvoke(callargs);
+                return @delegate.DynamicInvoke([.. objargs]);
             }
             else
             {
                 throw new ArgumentException($"Method {nameof(@delegate)} not exist.");
             }
+        }
+        private static bool ParameterMatch(ParameterInfo[] parameters, string[] strargs, List<object> objargs)
+        {
+            objargs.Clear();
+
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                Type paramType = parameters[i].ParameterType;
+
+                if (paramType != typeof(string) && paramType != typeof(string[]))
+                {
+                    return false;
+                }
+            }
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                Type paramType = parameters[i].ParameterType;
+
+                if (paramType == typeof(string[]))
+                {
+                    if (i < strargs.Length)
+                    {
+                        for (int j = i; j < strargs.Length; j++)
+                        {
+                            objargs.Add(strargs[j]);
+                        }
+                    }
+                    else
+                    {
+                        objargs.Add(Array.Empty<string>());
+                    }
+                    return true;
+                }
+                else if (i < strargs.Length)
+                {
+                    objargs.Add(strargs[i]);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
